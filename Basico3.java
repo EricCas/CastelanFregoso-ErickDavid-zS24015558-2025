49afe87290891cc5c3ae2f18d2785850237407aa
EricCas <avechunas2016@gmail.com>
2025-10-23 23:25:58 -0600

    Add files via upload
    Ejercicios para examen

---
+++ Basico3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Basico3{
+    public static void main(String[] args) {
+     int edad;
+     Scanner sc= new Scanner(System.in);
+     System.out.println("Introduce tu edad para calcular las pulsaciones: ");
+     edad = sc.nextInt();
+     double pulsaciones = (220.0 - edad) / 10.0;
+     System.out.println("El número de pulsaciones por cada 10 segundos debe de ser: " + pulsaciones);
+
+     sc.close();
+    }
+}
